e6d196c7256357942945930bab1cf669b2743ec6
Daniel Harris <bobth3bum@gmail.com>
2015-05-04 09:29:58 -0600

    second exercise

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,41 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
+    if (string1.length == string2.length) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  firstGreater: function(int1, int2) {
+    if (int1 > int2) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  firstGreaterThanOrEqualTo: function(int1, int2) {
+    if (int1 >= int2) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  firstLengthGreater: function(array1, array2) {
+    if (array1.length > array2.length) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  secondGreater: function(int1, int2) {
+    if (int2 > int1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
